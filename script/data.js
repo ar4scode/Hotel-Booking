c122f4cdacb28759196ba22e83707d9d9306626b
Ar4sCode <ar4sazizi@gmail.com>
2024-10-26 13:42:58 +0330

    Download top 3 hotel in london images and create data.js to save all images url

---
+++ script/data.js
@@ -0,0 +1,26 @@
+const hotels = [
+  {
+    hotelName: "The Royal Horseguards Hotel",
+    hotelAddress: "London, United Kingdom",
+    hotelSingleRoom: "../images/royal-horseguard-doubleroom.jpg",
+    hotelDoubleRoom: "../images/royal-horsegaurd-familyroom.jpg",
+    hotelBuilding: "../images/royal-horsegaurd-building.jpg",
+    hotelHallway: "../images/royal-horsegaurd-hallway.jpg",
+  },
+  {
+    hotelName: "The Savoy Hotel",
+    hotelAddress: "London, United Kingdom",
+    hotelSingleRoom: "../images/savoy-singleroom.jpg",
+    hotelDoubleRoom: "../images/savoy-double.webp",
+    hotelBuilding: "../images/savoy-building.jpg",
+    hotelHallway: "../images/savoy-hallway.jpg",
+  },
+  {
+    hotelName: "The Waldorf Hilton Hotel",
+    hotelAddress: "London, United Kingdom",
+    hotelSingleRoom: "../images/waldorf-single.jpg",
+    hotelDoubleRoom: "../images/waldorf-double.jpg",
+    hotelBuilding: "../images/waldorf-building.jfif",
+    hotelHallway: "../images/waldorf-hallway.avif",
+  },
+];
